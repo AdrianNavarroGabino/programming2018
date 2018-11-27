9768995225885ed10eb74bef18b385a69f0eade7
Adrián Navarro <adnaga94@gmail.com>
2018-11-27 10:13:12 +0100

    Exercise 003 - Two Lines

---
+++ 01-contactWithCSharp/003-TwoLines.cs
@@ -0,0 +1,11 @@
+// Message in two lines
+// Adrián Navarro Gabino
+
+public class Module
+{
+	public static void Main()
+	{
+		System.Console.WriteLine("This is");
+		System.Console.WriteLine("easy!!!");
+	}
+}
