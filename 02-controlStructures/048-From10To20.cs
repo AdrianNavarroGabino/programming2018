9ea2316403e610b216895a4669e648ef9446a50b
Adrián Navarro <adnaga94@gmail.com>
2018-11-27 12:21:39 +0100

    Exercise 048 - Display 10 to 20 (for)

---
+++ 02-controlStructures/048-From10To20.cs
@@ -0,0 +1,13 @@
+// Display 10 to 20 (for)
+// Adrián Navarro Gabino
+
+using System;
+
+public class From10To20
+{
+    public static void Main()
+    {
+        for(int i = 10; i <= 20; i++)
+            Console.WriteLine(i);
+    }
+}
