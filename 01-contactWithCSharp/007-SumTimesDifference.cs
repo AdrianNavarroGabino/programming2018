2e4ce6092fc34480e491ab8496c761eaf0df5e06
Adrián Navarro <adnaga94@gmail.com>
2018-11-27 10:23:21 +0100

    Exercise 007 - Sum times difference

---
+++ 01-contactWithCSharp/007-SumTimesDifference.cs
@@ -0,0 +1,14 @@
+// Adrián Navarro Gabino
+
+using System;
+
+public class  SumTimesDifference
+{
+	public static void Main()
+	{
+        Console.Write("(3 + 5) x (3 - 5) = ");
+		Console.WriteLine((3 + 5) * (3 - 5));
+        Console.Write("15 x 8 = ");
+		Console.WriteLine(15 * 8);
+	}
+}
