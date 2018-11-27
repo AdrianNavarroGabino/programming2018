0fb14e1a6106ad823f3c2e34b7d2f53586b93278
Adrián Navarro <adnaga94@gmail.com>
2018-11-27 11:36:49 +0100

    Exercise 028 - Positive, negative or zero?

---
+++ 02-controlStructures/028-PositiveNegativeOrZero.cs
@@ -0,0 +1,20 @@
+// Is a number positive, negative or zero?
+// Adrián Navarro Gabino
+
+using System;
+
+public class PositiveNegativeOrZero
+{
+	public static void Main()
+	{
+		Console.Write("Enter a number: ");
+		int num = Convert.ToInt32(Console.ReadLine());
+		
+		if(num > 0)
+			Console.WriteLine("Positive");
+		else if(num < 0)
+			Console.WriteLine("Negative");
+		else
+			Console.WriteLine("Zero");
+	}
+}
