41ee9020b825ce03d18875789680444e3b481641
Adrián Navarro <adnaga94@gmail.com>
2018-11-27 10:32:54 +0100

    Exercise 009 - Precedence of operators

---
+++ 01-contactWithCSharp/009-Precendence.cs
@@ -0,0 +1,14 @@
+// Adrián Navarro Gabino
+
+using System;
+
+public class Precedence
+{
+	public static void Main()
+	{
+		Console.WriteLine(-1+3*5);
+		Console.WriteLine((24+5)%7);
+		Console.WriteLine(15+-4*6/11);
+		Console.WriteLine(2+10/6*1-7%2);
+	}
+}
