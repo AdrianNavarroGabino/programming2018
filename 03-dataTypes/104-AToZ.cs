d0e1001cec9b0d4909ded2f7cb790b6f318403bf
Adrián Navarro <adnaga94@gmail.com>
2018-11-27 17:43:03 +0100

    Exercise 104 - "char" + "for"

---
+++ 03-dataTypes/104-AToZ.cs
@@ -0,0 +1,13 @@
+// Adrián Navarro Gabino
+
+using System;
+
+public class AToZ
+{
+    public static void Main()
+    {
+        for(char i = 'A'; i < 'Z'; i++)
+            Console.Write(i);
+        Console.WriteLine();
+    }
+}
