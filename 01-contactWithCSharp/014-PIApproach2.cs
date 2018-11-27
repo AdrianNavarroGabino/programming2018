6cf71cee1b3c57b831022e38484b01d05db9643b
Adrián Navarro <adnaga94@gmail.com>
2018-11-27 10:39:24 +0100

    Exercise 014: Pi as 22/7.0

---
+++ 01-contactWithCSharp/014-PIApproach2.cs
@@ -0,0 +1,13 @@
+// Approaching PI (Part 2)
+// Adrián Navarro Gabino
+
+using System;
+
+public class PIApproach2
+{
+	public static void Main()
+	{
+		Console.Write("Pi is aproximately ");
+		Console.WriteLine(22/7.0); 
+	}
+}
