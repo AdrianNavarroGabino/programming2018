7cd0625341211956e5a1f87fa517715df64ab011
Adrián Navarro Gabino <adnaga94@gmail.com>
2018-12-29 00:02:28 +0100

    Exercise 255 - Class TitleUnderlined + constructor

---
+++ 06-classes/255-TitleTest5.cs
@@ -0,0 +1,55 @@
+/*
+Create a constructor for the "Title" class, which will set the following 
+starting values: text = "Hello", x = 40, y = 10
+
+Test it from "Main"
+*/
+
+// Adrián Navarro Gabino
+
+using System;
+
+public class Title
+{
+	protected string text;
+	protected int x, y;
+    
+    public void SetX(int a) { x = a; }
+    public void SetY(int a) { y = a; }
+    public void SetText(string a) { text = a; }
+    
+    public Title()
+	{
+		x = 40;
+		y = 10;
+		text = "Hello";
+	}
+	
+	public void Show()
+	{
+		Console.SetCursorPosition(x, y);
+		Console.WriteLine(text);
+	}
+}
+
+public class TitleUnderline: Title
+{	
+	public void Show()
+	{
+		Console.SetCursorPosition(x, y);
+		Console.WriteLine(text);
+		Console.SetCursorPosition(x, y + 1);
+		string underline = new string('-', text.Length);
+		Console.WriteLine(underline);
+	}
+}
+
+public class TitleTest5
+{
+	public static void Main()
+	{
+		Title t = new Title();
+		
+		t.Show();
+	}
+}
