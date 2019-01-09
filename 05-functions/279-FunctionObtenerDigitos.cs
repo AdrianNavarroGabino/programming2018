a1aeefa8193a3e322a89b74b462f5016ae6fe94f
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-01-09 19:50:40 +0100

    Exercise 279 - Function ObtenerDigitos

---
+++ 05-functions/279-FunctionObtenerDigitos.cs
@@ -0,0 +1,36 @@
+// Adrián Navarro Gabino
+
+using System;
+
+public class FunctionObtenerDigitos
+{
+    public static void ObtenerDigitos(
+                                double n, ref int cEnt, ref int CMS)
+    {
+        CMS = 0;
+        
+        int num = (int)n;
+        int count = 0;
+        
+        while(num != 0)
+        {
+            num /= 10;
+            if(num < 10 && num > 0)
+                CMS = num;
+            count++;
+        }
+        
+        cEnt = count;
+    }
+    
+    public static void Main()
+    {
+        int cEnt = 0;
+        int CMS = 0;
+        
+        ObtenerDigitos(236.78, ref cEnt, ref CMS);
+        
+        Console.WriteLine(cEnt);
+        Console.WriteLine(CMS);
+    }
+}
