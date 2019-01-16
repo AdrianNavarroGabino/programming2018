10795c5d5fd3f849c880a0d0a5f95018342fb80d
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-01-16 15:58:27 +0100

    Exercise 287 - Class Book

---
+++ 06-classes/287-ClassBook.cs
@@ -0,0 +1,29 @@
+// Adrián Navarro Gabino
+
+using System;
+
+class Book
+{
+    private string title;
+    private string author;
+    private string location;
+    
+    public Book(string newTitle, string newAuthor,
+        string newLocation)
+    {
+        title = newTitle;
+        author = newAuthor;
+        location = newLocation;
+    }
+    
+    public void SetTitle(string newTitle) { title = newTitle; }
+    public void SetAuthor(string newAuthor) { author = newAuthor; }
+    public void SetLocation(string newLocation)
+    {
+        location = newLocation;
+    }
+    
+    public string GetTitle() { return title; }
+    public string GetAuthor() { return author; }
+    public string GetLocation() { return location; }
+}
