a4c8b6b8351bf9a02e9fa4c8833e78b5ef319f7f
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-01-18 16:48:28 +0100

    SDL Project: BubbleBobble 0.05 (room)

--- projects/bubbleBobble/inUse/BubbleBobble/Game.cs
+++ projects/bubbleBobble/inUse/BubbleBobble/Game.cs
@@ -9,47 +9,21 @@ using System.Threading.Tasks;
 
 class Game
 {
-    /*
-    struct typeEnemy
-    {
-        public int x;
-        public int y;
-        public int speed;
-    }
-    */
-
-    //static Image player;
-    //static int playerX, playerY, playerSpeed;
-    //static int playerWidth, playerHeight;
-    static Player player;
-
-    static int numEnemies;
-    //static Image enemy;
-    //static int enemyWidth;
-    //static int enemyHeight;
-    static Enemy[] enemies;
-
-    static bool finished;
+    protected Player player;
+    protected int numEnemies;
+    protected Enemy[] enemies;
+    protected Room room;
+    protected bool finished;
+    protected Font font18;
 
         
-    public static void Init()
+    void Init()
     {
-
-
-        //player = new Image("data/player.png");
-        //playerX = 50;
-        //playerY = 120;
-        //playerSpeed = 8;
-        //playerWidth = 32;
-        //playerHeight = 64;
         player = new Player();
 
         numEnemies = 2;
         enemies = new Enemy[numEnemies];
-            
-        //enemy = new Image("data/enemy.png");
-        //enemyWidth = 64;
-        //enemyHeight = 64;
+        
         for(int i = 0; i < numEnemies; i++)
         {
             enemies[i] = new Enemy();
@@ -63,61 +37,58 @@ class Game
             enemies[i].MoveTo(rnd.Next(200, 800), rnd.Next(50, 600));
             enemies[i].SetSpeed(rnd.Next(1, 5), rnd.Next(1, 5));
         }
+
+        font18 = new Font("data/Joystix.ttf", 18);
+        room = new Room();
     }
 
-    static void UpdateScreen()
+    void UpdateScreen()
     {
-        Font font18 = new Font("data/Joystix.ttf", 18);
         SdlHardware.ClearScreen();
+        room.DrawOnHiddenScreen();
 
         SdlHardware.WriteHiddenText("Score: ",
             40, 10,
             0xCC, 0xCC, 0xCC,
             font18);
-
-        //SdlHardware.DrawHiddenImage(player, playerX, playerY);
+        
         player.DrawOnHiddenScreen();
         for (int i = 0; i < numEnemies; i++)
-            //SdlHardware.DrawHiddenImage(enemy, enemies[i].x, enemies[i].y);
             enemies[i].DrawOnHiddenScreen();
         SdlHardware.ShowHiddenScreen();
     }
 
-    static void CheckUserInput()
+    void CheckUserInput()
     {
         if (SdlHardware.KeyPressed(SdlHardware.KEY_RIGHT))
-            //playerX += playerSpeed;
             player.MoveRight();
         if (SdlHardware.KeyPressed(SdlHardware.KEY_LEFT))
-            //playerX -= playerSpeed;
             player.MoveLeft();
         if (SdlHardware.KeyPressed(SdlHardware.KEY_UP))
-            //playerY -= playerSpeed;
             player.MoveUp();
         if (SdlHardware.KeyPressed(SdlHardware.KEY_DOWN))
-            //playerY += playerSpeed;
             player.MoveDown();
 
         if (SdlHardware.KeyPressed(SdlHardware.KEY_ESC))
             finished = true;
     }
 
-    static void UpdateWorld()
+    void UpdateWorld()
     {
         // TODO
     }
 
-    static void CheckGameStatus()
+    void CheckGameStatus()
     {
         // TODO
     }
 
-    static void PauseUntilNextFrame()
+    void PauseUntilNextFrame()
     {
         SdlHardware.Pause(40);
     }
 
-    public static void UpdateHighscore()
+    void UpdateHighscore()
     {
         // TODO
     }
---
+++ projects/bubbleBobble/inUse/BubbleBobble/Room.cs
@@ -0,0 +1,48 @@
+﻿class Room
+{
+    protected Image wallL1;
+
+    protected int mapHeight = 16, mapWidth = 24;
+    protected int tileWidth = 32, tileHeight = 32;
+    protected int leftMargin = 100, topMargin = 50;
+
+    protected string[] levelData =
+    {
+        "111111111111111111111111",
+        "11                    11",
+        "11                    11",
+        "11                    11",
+        "11                    11",
+        "1111  111111111111  1111",
+        "11                    11",
+        "11                    11",
+        "1111  111111111111  1111",
+        "11                    11",
+        "11                    11",
+        "1111  111111111111  1111",
+        "11                    11",
+        "11                    11",
+        "11                    11",
+        "111111111111111111111111"};
+
+    public Room()
+    {
+        wallL1 = new Image("data/tile-wallL1.png");
+    }
+
+    public void DrawOnHiddenScreen()
+    {
+        for (int row = 0; row < mapHeight; row++)
+        {
+            for (int col = 0; col < mapWidth; col++)
+            {
+                int posX = col * tileWidth + leftMargin;
+                int posY = row * tileHeight + topMargin;
+                switch (levelData[row][col])
+                {
+                    case '1': SdlHardware.DrawHiddenImage(wallL1, posX, posY); break;
+                }
+            }
+        }
+    }
+}
