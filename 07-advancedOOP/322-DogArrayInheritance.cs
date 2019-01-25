a56c90ee9014409d69c6497bae1c451d3dbaf718
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-01-25 10:23:53 +0100

    Exercise 322 - Array of objects Dog + new

---
+++ 07-advancedOOP/322-DogArrayInheritance.cs
@@ -0,0 +1,44 @@
+// Adrián Navarro Gabino
+
+using System;
+
+class Dog
+{
+    public void Bark()
+    {
+        Console.Write("Woof");
+    }
+}
+
+class Dalmatian : Dog
+{
+    public new void Bark()
+    {
+        Console.Write("Aww");
+    }
+}
+
+class DogTest
+{
+    static void Main()
+    {
+        const int SIZE = 3;
+        
+        Dog[] d = new Dog[SIZE];
+        
+        for(int i = 0; i < SIZE - 1; i++)
+        {
+            d[i] = new Dog();
+        }
+        
+        d[2] = new Dalmatian();
+        
+        for(int i = 0; i < SIZE - 1; i++)
+        {
+            d[i].Bark();
+            Console.WriteLine();
+        }
+        
+        d[2].Bark();
+    }
+}
