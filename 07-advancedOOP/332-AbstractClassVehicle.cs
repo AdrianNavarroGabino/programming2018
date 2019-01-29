75781c96bf67ee08c0ab3599c5bf9b143840cd64
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-01-29 17:41:15 +0100

    Exercise 332 - Abstract class Vehicle

---
+++ 07-advancedOOP/332-AbstractClassVehicle.cs
@@ -0,0 +1,75 @@
+// Adrián Navarro Gabino
+
+using System;
+
+abstract class Vehicle
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public double Power { get; set; }
+    public int Speed { get; set; }
+    
+    public Vehicle(string newBrand, string newModel, double newPower)
+    {
+        Brand = newBrand;
+        Model = newModel;
+        Power = newPower;
+        Speed = 0;
+    }
+    
+    public void Start()
+    {
+        Console.WriteLine("The vehicle is ready to go");
+    }
+    
+    public void Accelerate()
+    {
+        Speed += 5;
+    }
+    
+    public void Brake()
+    {
+        Speed -= 5;
+    }
+    
+    public override string ToString()
+    {
+        return "Brand: " + Brand + "\nModel: " + Model + "\nPower: " +
+            Power + "HP" + "\nSpeed: " + Speed + "km/h";
+    }
+}
+
+class Car : Vehicle
+{
+    public byte Doors { get; set; }
+    
+    public Car(string newBrand, string newModel, double newPower,
+        byte newDoors) : base(newBrand, newModel, newPower)
+    {
+        Doors = newDoors;
+    }
+}
+
+class Motorbike : Vehicle
+{
+    public string TypeOfLicense { get; set; }
+    
+    public Motorbike(string newBrand, string newModel, double newPower,
+        string typeOfLicense) : base(newBrand, newModel, newPower)
+    {
+        TypeOfLicense = typeOfLicense;
+    }
+}
+
+class CarTest
+{
+    static void Main()
+    {
+        Car myCar = new Car("Brand","Model",1000,4);
+        
+        myCar.Start();
+        
+        // It doesn't work because the class is abstract
+        Vehicle v = new Vehicle("Brand","Model",1000);
+    }
+}
