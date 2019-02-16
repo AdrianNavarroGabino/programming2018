9491bc2ec34a78d8bb28db9a11123f3cd8a1bdb7
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-02-16 21:49:06 +0100

    Exercise 359 - Contact with ArrayList

---
+++ 08-dynamicMemory/359-ArrayListIntroduction.cs
@@ -0,0 +1,28 @@
+// Adrián Navarro Gabino
+
+using System;
+using System.Collections;
+
+class ArrayListIntroduction
+{
+    static void Main()
+    {
+        ArrayList myList = new ArrayList();
+        
+        string sentence;
+        
+        do
+        {
+            Console.Write("Enter a sentence: ");
+            sentence = Console.ReadLine();
+            
+            if(sentence != "")
+                myList.Add(sentence);
+        } while(sentence != "");
+        
+        for(int i = myList.Count - 1; i >= 0; i--)
+        {
+            Console.WriteLine(myList[i]);
+        }
+    }
+}
