6212fea633c90aa1c065a7f6d465bf167d627f4f
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-02-21 18:51:01 +0100

    Exercise 367 - Queue of longs

---
+++ 08-dynamicMemory/367-QueueOfLong.cs
@@ -0,0 +1,27 @@
+// Adrián Navarro Gabino
+
+using System;
+using System.Collections.Generic;
+
+class QueueOfLong
+{
+    static void Main()
+    {
+        Queue<long> data = new Queue<long>();
+
+        string text;
+        do
+        {
+            text = Console.ReadLine();
+            if (text != "")
+            {
+                data.Enqueue(Convert.ToInt64(text));
+            }
+        } while (text != "");
+
+        while(data.Count > 0)
+        {
+            Console.WriteLine(data.Dequeue());
+        }
+    }
+}
