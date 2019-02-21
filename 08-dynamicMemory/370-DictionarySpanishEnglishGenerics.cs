a13efa651c11b70e74a466a32f4982c0b4210e71
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-02-21 18:48:18 +0100

    Exercise 370 - Dictionary - Generics

---
+++ 08-dynamicMemory/370-DictionarySpanishEnglishGenerics.cs
@@ -0,0 +1,32 @@
+// Adrián Navarro Gabino
+
+using System;
+using System.Collections.Generic;
+
+class DictionarySpanishEnglishDictionary
+{
+    static void Main()
+    {
+        Dictionary<string, string> myDictionary =
+            new Dictionary<string, string>();
+        
+        myDictionary.Add("hola", "hello");
+        myDictionary.Add("adiós", "good bye");
+        myDictionary.Add("rojo", "red");
+        myDictionary.Add("azul", "blue");
+        myDictionary.Add("perro", "dog");
+        myDictionary.Add("gato", "cat");
+        myDictionary.Add("casa", "house");
+        myDictionary.Add("cocina", "kitchen");
+        myDictionary.Add("amarillo", "yellow");
+        myDictionary.Add("mascota", "pet");
+        
+        Console.Write("Enter the word in spanish to translate: ");
+        string word = Console.ReadLine().ToLower();
+        
+        if(myDictionary.ContainsKey(word))
+            Console.WriteLine(myDictionary[word]);
+        else
+            Console.WriteLine("Not found");
+    }
+}
