270a0c66339be236402dde7d53256e224cc1b24d
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-02-26 11:55:08 +0100

    SDL Project: BubbleBobble 0.08 (gravity, jump)

--- projects/bubbleBobble/inUse/BubbleBobble/Player.cs
+++ projects/bubbleBobble/inUse/BubbleBobble/Player.cs
@@ -1,5 +1,11 @@
 ﻿class Player : Sprite
 {
+    protected bool jumping = false, falling = false;
+    protected int xJumpSpeed = 0;
+    int[] stepsOfJump = {-34, -27, -22, -16, -10, -6, -2, -1, 0,
+                               0, 1, 2, 6, 10, 16, 22, 27, 34};
+    protected int frameOfTheJump = 0;
+
     public Player()
     {
         //LoadImage("data/player.png");
@@ -17,27 +23,100 @@
         height = 48;
     }
 
-    public void MoveRight()
+    public void MoveRight(Room room)
     {
-        x += xSpeed;
-        ChangeDirection(RIGHT);
-        NextFrame();
+        if (room.CanMoveTo(x + xSpeed,
+                    y,
+                    x + width + xSpeed,
+                    y + height))
+        {
+            x += xSpeed;
+            ChangeDirection(RIGHT);
+            NextFrame();
+        }
     }
 
-    public void MoveLeft()
+    public void MoveLeft(Room room)
     {
-        x -= xSpeed;
-        ChangeDirection(LEFT);
-        NextFrame();
+        if (room.CanMoveTo(x - xSpeed,
+            y,
+            x + width - xSpeed,
+            y + height))
+        {
+            x -= xSpeed;
+            ChangeDirection(LEFT);
+            NextFrame();
+        }
     }
 
-    public void MoveUp()
+    public void Jump(Room room)
     {
-        y -= ySpeed;
+        if (jumping || falling)
+            return;
+        jumping = true;
+        xJumpSpeed = 0;
     }
 
-    public void MoveDown()
+    public void JumpRight(Room room)
     {
-        y += ySpeed;
+        Jump(room);
+        xJumpSpeed = xSpeed;
+    }
+
+
+    // Starts the jump left sequence
+    public void JumpLeft(Room room)
+    {
+        Jump(room);
+        xJumpSpeed = -xSpeed;
+    }
+
+    // Used when the player must move on his own, e.g. jumping
+    public void Move(Room room)
+    {
+        if (jumping)
+        {
+            short xNextMove = (short)(x + xJumpSpeed);
+            short yNextMove = (short)(y + stepsOfJump[frameOfTheJump]);
+            
+            if (room.CanMoveTo(
+                    xNextMove,
+                    yNextMove,
+                    xNextMove + width,
+                    yNextMove + height)
+                )
+            {
+                x = xNextMove;
+                y = yNextMove;
+                NextFrame();
+            }
+            else
+            {
+                jumping = false;
+                falling = true;
+            }
+
+            frameOfTheJump++;
+            if (frameOfTheJump >= stepsOfJump.Length)
+            {
+                jumping = false;
+                falling = true;
+                frameOfTheJump = 0;
+            }
+        }
+        else
+        {
+            if (room.CanMoveTo(
+                x,
+                y + ySpeed,
+                x + width,
+                y + ySpeed + height))
+            {
+                y += ySpeed;
+                falling = true;
+            }
+            else
+                falling = false;
+        }
     }
 }
