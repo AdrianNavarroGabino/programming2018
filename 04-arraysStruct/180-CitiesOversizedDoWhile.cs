0e46a205faf28a728b2b5ad58c12f1797bff9225
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-01 11:52:19 +0100

    Exercise 180 - Oversized array of struct city

--- 04-arraysStruct/180-CitiesOversizedDoWhile.cs
+++ 04-arraysStruct/180-CitiesOversizedDoWhile.cs
@@ -2,7 +2,7 @@
 
 using System;
 
-public class Cities2
+public class Cities
 {
     struct city
     {
@@ -24,8 +24,6 @@ public class Cities2
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1. Add a city");
             Console.WriteLine("2. See all cities");
-            Console.WriteLine("3. Modify a city");
-            Console.WriteLine("4. Search a city");
             Console.WriteLine("0. Exit");
             option = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
@@ -58,49 +56,6 @@ public class Cities2
                     
                     Console.WriteLine();
                 }
-            }
-            
-            else if(option == 3)
-            {
-                Console.Write("Enter the city number: ");
-                int cityNumber = Convert.ToInt32(Console.ReadLine()) - 1;
-                
-                Console.Write("Enter the name (it was {0}): ",
-                    c[cityNumber].name);
-                string newName = Console.ReadLine();
-                if(newName != "")
-                {
-                    c[cityNumber].name = newName;
-                }
-                
-                Console.Write("Enter the population (it was {0}): ",
-                    c[cityNumber].population);
-                string newPopulation = Console.ReadLine();
-                if(newPopulation != "")
-                {
-                    c[cityNumber].population = Convert.ToInt32(newPopulation);
-                }
-            }
-            
-            else if(option == 4)
-            {
-                Console.WriteLine("Enter the words you want to search:");
-                string wordsToSearch = Console.ReadLine().ToUpper();
-                
-                Console.WriteLine();
-                
-                for(int i = 0; i < count; i++)
-                {
-                    if(c[i].name.ToUpper().Contains(wordsToSearch))
-                    {
-                        Console.WriteLine("City number " + (i+1) + ":");
-                        Console.WriteLine("Name: " + c[i].name);
-                        Console.WriteLine("Population: " + c[i].population);
-                        
-                        Console.WriteLine();
-                    }
-                }
-                
             }
         } while(option != 0);
     }
