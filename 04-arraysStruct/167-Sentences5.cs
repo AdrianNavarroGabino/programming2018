a9efcd5fb41764d6af68d97b221df15c8b10bdab
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-01 11:28:50 +0100

    Exercise 167 - String to uppercase

---
+++ 04-arraysStruct/167-Sentences5.cs
@@ -0,0 +1,14 @@
+// Adrián Navarro Gabino
+
+using System;
+
+public class Sentences5
+{
+    public static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine(name.ToUpper());
+    }
+}
