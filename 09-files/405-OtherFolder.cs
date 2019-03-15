28defc7323eb7523349e98fdde8a95f287333e26
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-15 11:16:42 +0100

    Exercise 405 - File in another folder

---
+++ 09-files/405-OtherFolder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader csproj = new StreamReader("..\\..\\Program.csproj");
+            StreamReader sln = new StreamReader(@"..\..\..\Program.sln");
+
+            string line;
+
+            Console.WriteLine("Program.csproj");
+            Console.WriteLine();
+
+            do
+            {
+                line = csproj.ReadLine();
+
+                if (line != null)
+                    Console.WriteLine(line);
+            } while (line != null);
+
+            Console.WriteLine();
+            Console.WriteLine("Program.sln");
+            Console.WriteLine();
+
+            do
+            {
+                line = sln.ReadLine();
+
+                if (line != null)
+                    Console.WriteLine(line);
+            } while (line != null);
+        }
+    }
+}
