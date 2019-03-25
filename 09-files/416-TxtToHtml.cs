7fb0507154b27f5be9e1132e08ac4839ef9eec63
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-25 10:44:53 +0100

    Exercise 416 - Txt to Html

---
+++ 09-files/416-TxtToHtml.cs
@@ -0,0 +1,49 @@
+// Adrián Navarro Gabino
+
+using System;
+using System.IO;
+
+class TxtToHtml
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("File name? ");
+        string name = Console.ReadLine();
+
+        StreamReader txtFile = new StreamReader(name);
+        StreamWriter htmlFile = new StreamWriter(name + ".html");
+
+        htmlFile.WriteLine("<html>");
+        htmlFile.WriteLine("<body>");
+
+        string lastLine = "";
+        string line = txtFile.ReadLine();
+        bool inParagraph = false;
+        while (line != null)
+        {
+            if (line == "")
+                htmlFile.WriteLine("</p>");
+            else
+            {
+                if (lastLine == "")
+                    htmlFile.WriteLine("<p>" + line);
+                else
+                    htmlFile.WriteLine("<br />" + line);
+            }
+
+            lastLine = line;
+            line = txtFile.ReadLine();
+        }
+        
+        if ( inParagraph )
+        {
+            htmlFile.WriteLine("</p>");
+        }    
+
+        htmlFile.WriteLine("</body>");
+        htmlFile.WriteLine("</html>");
+
+        htmlFile.Close();
+        txtFile.Close();
+    }
+}
